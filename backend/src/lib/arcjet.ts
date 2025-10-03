29eeb551da84cfedf60941b9b31b8b907a259fca
krisn0x <10799186+krisn0x@users.noreply.github.com>
2025-10-03 23:02:51 +0300

    add arcjet middleware

---
+++ backend/src/lib/arcjet.ts
@@ -0,0 +1,37 @@
+import arcjet, {tokenBucket, shield, detectBot} from "@arcjet/node"
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+export const aj = arcjet({
+  // Get your site key from https://app.arcjet.com and set it as an environment
+  // variable rather than hard coding.
+  key: process.env.ARCJET_KEY!,
+  characteristics: ["ip.src"],
+  rules: [
+    // Shield protects your app from common attacks e.g. SQL injection
+    shield({ mode: "LIVE" }),
+    // Create a bot detection rule
+    detectBot({
+      mode: "DRY_RUN", // Blocks requests. Use "DRY_RUN" to log only
+      // Block all bots except the following
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        // Uncomment to allow these other common bot categories
+        // See the full list at https://arcjet.com/bot-list
+        //"CATEGORY:MONITOR", // Uptime monitoring services
+        //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+      ],
+    }),
+    // Create a token bucket rate limit. Other algorithms are supported.
+    tokenBucket({
+      mode: "LIVE",
+      // Tracked by IP address by default, but this can be customized
+      // See https://docs.arcjet.com/fingerprints
+      //characteristics: ["ip.src"],
+      refillRate: 10, // Refill 5 tokens per interval
+      interval: 20, // Refill every 10 seconds
+      capacity: 20, // Bucket capacity of 10 tokens
+    }),
+  ],
+});
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,8 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import productRoutes from './routes/productRoutes.js'
 import { sql } from './config/db.js'
+import { aj } from './lib/arcjet.js'
+import { ArcjetNodeRequest } from '@arcjet/node'
 
 dotenv.config()
 const app = express()
@@ -15,8 +17,40 @@ app.use(cors()) // allows cross-origin requests
 app.use(helmet()) // adds security via headers
 app.use(morgan('dev')) //logs requests
 
-app.use('/api/products', productRoutes)
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req as ArcjetNodeRequest, {
+      requested: 5,
+    }) // Deduct 5 tokens from the bucket
+    console.log('Arcjet decision', decision)
 
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.writeHead(429, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ error: 'Too Many Requests' }))
+      } else if (decision.reason.isBot()) {
+        res.writeHead(403, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ error: 'No bots allowed' }))
+      } else {
+        res.writeHead(403, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ error: 'Forbidden' }))
+      }
+    } else if (decision.ip.isHosting()) {
+      // Requests from hosting IPs are likely from bots, so they can usually be
+      // blocked. However, consider your use case - if this is an API endpoint
+      // then hosting IPs might be legitimate.
+      // https://docs.arcjet.com/blueprints/vpn-proxy-detection
+      res.writeHead(403, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ error: 'Forbidden' }))
+    } else {
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.use('/api/products', productRoutes)
 
 initDB().then(() => {
   app.listen(PORT, () => {

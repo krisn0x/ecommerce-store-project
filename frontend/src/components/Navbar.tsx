f2779b8538c0bb0b30d150814dd542f317ed986e
krisn0x <10799186+krisn0x@users.noreply.github.com>
2025-10-05 00:30:15 +0300

    fix typos

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -21,7 +21,7 @@ function Navbar() {
                   className="font-semibold font-mono tracking-widest text-2xl 
                     bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
                 >
-                  POSGRESTORE
+                  POSTGRESTORE
                 </span>
               </div>
             </Link>

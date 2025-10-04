a356747ff78f518f6e0bc345bb7fcf3e82b5ccf7
krisn0x <10799186+krisn0x@users.noreply.github.com>
2025-10-04 16:16:40 +0300

    remove React strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot, type Container } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root') as Container).render(
-  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
 )
